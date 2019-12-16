242160e3810b1bef1fba616644273bc7a61b3070
rudalson <rudalson@gmail.com>
2019-12-16 16:27:30 +0900

    added the drawer's sub-item of vuetify

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,49 @@
         <v-toolbar-title>Account</v-toolbar-title>
       </v-toolbar>
       <v-divider></v-divider>
-      <v-list two-line>
-        <v-list-tile v-for="item in items" :key="item.title" :to="item.to">
+      <v-list>
+        <v-list-group
+            v-for="item in items"
+            :key="item.title"
+            v-model="item.active"
+            :prepend-icon="item.icon"
+            no-action
+          >
+          <template v-slot:activator>
+            <v-list-tile>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+
+          <v-list-tile
+            v-for="subItem in item.subItems"
+            :key="subItem.title"
+            :to="subItem.to"
+          >
+            <v-list-tile-content>
+              <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-icon>{{ subItem.action }}</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
+        <!-- <v-list-tile v-for="item in items" :key="item.title" :to="item.to">
           <v-list-tile-avatar>
             <v-icon>{{item.icon}}</v-icon>
           </v-list-tile-avatar>
           <v-list-tile-content>
             <v-list-tile-title>{{item.title}}</v-list-tile-title>
-            <!--        <v-list-tile-sub-title>bbb</v-list-tile-sub-title>-->
           </v-list-tile-content>
           <v-list-tile-action>
             <v-btn icon ripple>
               <v-icon color="grey lighten-1">info</v-icon>
             </v-btn>
           </v-list-tile-action>
-        </v-list-tile>
+        </v-list-tile> -->
       </v-list>
     </v-navigation-drawer>
     <v-toolbar dark color="lime">
@@ -57,18 +85,33 @@ export default {
       items: [
         {
           icon: 'email',
-          title: 'home',
-          to: '/'
+          title: 'Home',
+          active: true,
+          subItems: [
+            {
+              title: 'dashboard',
+              to: '/'
+            },
+            {
+              title: 'about',
+              to: '/about/'
+            }
+          ]
         },
         {
           icon: 'search',
-          title: 'about',
-          to: '/about/'
-        },
-        {
-          icon: 'apps',
-          title: 'about2',
-          to: '/about2/'
+          title: 'Lectures',
+          active: false,
+          subItems: [
+            {
+              title: 'card',
+              to: '/lectures/card'
+            },
+            {
+              title: 'layout',
+              to: '/lectures/layout'
+            }
+          ]
         }
       ]
     }
