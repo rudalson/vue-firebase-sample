58d33d813cac0f680e51c1308e6273884c62665e
rudalson <rudalson@gmail.com>
2019-12-16 11:13:22 +0900

    added the toolbar list

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-list two-line>
+    <v-list-tile v-for="item in items" :key="item.title">
+      <v-list-tile-avatar>
+        <v-icon>{{item.icon}}</v-icon>
+      </v-list-tile-avatar>
+      <v-list-tile-content>
+        <v-list-tile-title>{{item.title}}</v-list-tile-title>
+<!--        <v-list-tile-sub-title>bbb</v-list-tile-sub-title>-->
+      </v-list-tile-content>
+      <v-list-tile-action>
+        <v-btn icon ripple>
+          <v-icon color="grey lighten-1">info</v-icon>
+        </v-btn>
+      </v-list-tile-action>
+    </v-list-tile>
+  </v-list>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          icon: 'email',
+          title: 'title1'
+        },
+        {
+          icon: 'search',
+          title: 'title2'
+        },
+        {
+          icon: 'apps',
+          title: 'title3'
+        }
+      ]
+    }
+  }
+}
+</script>
