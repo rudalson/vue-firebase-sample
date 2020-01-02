7ccd14a615c5937eb59c1eb7c2cdbe8be6559994
rudalson <rudalson@gmail.com>
2020-01-02 19:32:41 +0900

    added 47 파이어스토어 정렬해보기

--- src/views/admin/users.vue
+++ src/views/admin/users.vue
@@ -12,6 +12,7 @@
             :items-per-page="5"
             :loading="loading"
             class="elevation-1"
+            must-sort
           ></v-data-table>
         </v-card-text>
         <v-card-actions>
@@ -27,8 +28,7 @@ export default {
     return {
       totalCount: 0,
       items: [],
-      loading: true,
-      options: {},
+      loading: false,
       headers: [
         {
           text: '유저키',
@@ -36,8 +36,13 @@ export default {
         },
         { text: 'email', value: 'email' },
         { text: '이름', value: 'displayName' },
-        { text: 'photoURL', value: 'photoURL' }
-      ]
+        { text: 'photoURL', value: 'photoURL' },
+        { text: 'level', value: 'level' }
+      ],
+      options: {
+        sortBy: ['email'],
+        sortDesc: [false]
+      }
     }
   },
   watch: {
@@ -50,15 +55,19 @@ export default {
   },
   methods: {
     async list () {
+      this.loading = true
       const { data } = await this.$axios.get('/admin/users', {
         params: {
           offset: this.options.page > 0 ? (this.options.page - 1) * this.options.itemsPerPage : 0,
-          limit: this.options.itemsPerPage
+          limit: this.options.itemsPerPage,
+          order: this.options.sortBy[0],
+          sort: this.options.sortDesc[0] ? 'desc' : 'asc'
         }
       })
-      console.log(this.options)
       this.totalCount = data.totalCount
       this.items = data.items
+      this.loading = false
+      console.log(this.options)
     }
   }
 }
